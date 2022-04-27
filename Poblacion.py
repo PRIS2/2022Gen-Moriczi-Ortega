a0236aada60fb77862a4ea057f89ae9f9c09303c
HUNOR MORICZI <hm586@inlumine.ual.es>
2022-04-27 19:29:18 +0200

    clases terminadas desde repo propio para poder enlazar a codacy y Sonarcloud

--- Poblacion.py
+++ Poblacion.py
@@ -1,41 +1,51 @@
 #Declaro una lista de individuos
-import random
+import secrets
 import Individuo
 
 poblacion = []
-#Declaro una lista de individuos que se van a cruzar
-poblacionCruzada = []
-#Declaro una lista de individuos que se van a mutar
-poblacionMutacion = []
 #Declaro una lista de individuos que se van a cruzar y mutar
-poblacionCruzaMutacion = []
-
-
+poblacion_cruza_mutacion = []
 
 #metodo para rellenar la lista poblacion con x individuos
-def rellenarPoblacion(x):
+def rellenar_poblacion(x):
     for i in range(x):
-        #Declaro una lista de genes de tamaño 10 con valores entre 0 y 1
         genes = []
-        #bucle para rellenar la lista de genes con valores 0 o 1
-        for i in range(10): 
-            genes.append(random.randint(0,1))
-        #Creo un nuevo individuo con los genes de los padres cruzados
+        #Creo un nuevo individuo
         individuo = Individuo.individuo(genes)
+        individuo.rellenar_genes()
         poblacion.append(individuo)
 
 #creo una funcion para mostrar la poblacion
-def mostrarPoblacion():
-    for individuo in poblacion:
-        print(individuo.genes)  
+def mostrar_poblacion(lista_individuos):
+    for individuo in lista_individuos:
+        print(individuo.genes)
 
-rellenarPoblacion(10)
-mostrarPoblacion()
+#metodo que devuelve una lista con los individuos cruzados pasando una lista de individuos ademas
+# de una probabilidad de mutacion pasada por parametro
+def cruzar_mutar_individuos(lista_individuos, probabilidad):
+    punto = secrets.randbelow(10)
+    lista_cruza_mutacion = []
+    if probabilidad < 0.5:
+        print("Ha habido mutación en la poblacion ")
+    for individuo in lista_individuos:
+        random = secrets.randbelow(len(lista_individuos))
+        individuo_cruza = individuo.cruzar_punto(lista_individuos[random], punto)
+        if probabilidad < 0.5:
+            individuo_mutacion = individuo_cruza.mutar(probabilidad)
+            lista_cruza_mutacion.append(individuo_mutacion)
+        else:
+            lista_cruza_mutacion.append(individuo_cruza)
+    return lista_cruza_mutacion
 
-for individuo in poblacion:
-    if random.random() < 0.5:
-        poblacionCruzada.append(individuo)
-    else:
-        poblacionMutacion.append(individuo)
+#Pruebo el metodo de cruzarMutarIndividuos, generando una poblacion inicial de individuos.
+#Luego genero una lista con los individuos cruzados y mutados aleatoriamente
+#muestro la poblacion inicial y la poblacion cruzada y mutada
+rellenar_poblacion(10)
+random = secrets.randbelow(2)
 
-#Pendiente de hacer el cruzamiento y mutacion de los individuos de la poblacionCruzada y poblacionMutacion respectivamente a partir de la poblacion 
+print("Población inicial:")
+mostrar_poblacion(poblacion)
+print("\n")
+poblacion_cruza_mutacion = cruzar_mutar_individuos(poblacion,random)
+print("Población cruzada y mutada: ")
+mostrar_poblacion(poblacion_cruza_mutacion)
